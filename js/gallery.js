7666eea74dc163bb7bf62220a9cbe03e527ec2d4
shashwatshrye-ctrl <shashwatshrye@gmail.com>
2025-12-25 10:12:31 +0530

    Delete js directory

--- js/gallery.js
+++
@@ -1,8 +0,0 @@
-function openModal(src) {
-  document.getElementById("modal").style.display = "flex";
-  document.getElementById("modal-img").src = src;
-}
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
